7175a77d5cab1f15571866adeb22a673ce2cfca7
Sarah Schüller <sarah.schueller@infotip.de>
2018-10-04 15:04:40 +0200

    add undo()

--- Python Files/Program/Field.py
+++ Python Files/Program/Field.py
@@ -22,6 +22,18 @@ class Field(ABC):
     # changes current Player
         self._activePlayer = not self._activePlayer
 
+    def _undo(self):
+        if self.getActivePlayer() and len(self._red) > 0:
+            last = self._red[len(self._red)-1]
+            self._playground[last] = None
+            self._red.pop()
+            self.__changePlayer()
+        elif len(self._yellow) > 0:
+            last = self._yellow[len(self._yellow)-1]
+            self._playground[last] = None
+            self._yellow.pop()
+            self.__changePlayer()            
+
     def _setStone(self, column):
     # Sets Stone by given column: 
     # Stone falls until he hits the ground or another Stone
@@ -100,7 +112,8 @@ class GUI(Field):
 
     def __init__ (self): 
         Field.__init__(self) 
-        self.__restart = Actor('restart', (35,120))
+        self.__restart = Actor('restart', (540, 165))
+        self.__undo = Actor('back', (575, 165))
         
     def __drawRed(self, field):
     # draws a red Stone in given field
@@ -112,7 +125,7 @@ class GUI(Field):
     # draws a yellow Stone in given field
         y = 6 - math.ceil((field +1) / 7)
         x = field - math.floor((field) / 7)*7
-        screen.draw.filled_circle((60 + x * 70, 565 - 70 * y), 30, (255, 255, 0))
+        screen.draw.filled_circle((60 + x * 70, 565 - 70 * y), 30, (255,215,0))
 
     def draw(self):
     # draws field with current status
@@ -124,7 +137,7 @@ class GUI(Field):
 
         #draw empty playground
         blue = 0, 50, 200
-        box = Rect((20, 150), (HEIGHT - 100,450))        
+        box = Rect((20, HEIGHT-450), (500, 450))        
         screen.draw.filled_rect(box, blue)
 
         # circle radius = 30, space between = 10, startX = 60, startY = 565
@@ -139,13 +152,19 @@ class GUI(Field):
         for x in self._red:
             self.__drawRed(x)
 
+        self.__restart.draw()
+        self.__undo.draw()
+
         # give message if there is a winner
         if self._checkWinner() == False:
-            screen.draw.text("Red wins", (60, 115), color='red')
-            self.__restart.draw()
+            screen.draw.text("Red wins", (20, 115), color='red')
         elif self._checkWinner() == True:
-            screen.draw.text("Yellow wins", (60, 115), color='yellow')
-            self.__restart.draw()
+            screen.draw.text("Yellow wins", (20, 115), color=(255,215,0))
+        elif self.getActivePlayer():
+            screen.draw.text("Yellows Turn", (20, 115), color='black')
+        else:
+            screen.draw.text("Reds Turn", (20, 115), color='black')
+
 
     def clicked(self,pos):
     # as long as there is no winner add a stone to a column by clicking on the field in the first row
@@ -154,9 +173,12 @@ class GUI(Field):
                 if pos[1] > 180 and pos[1] < 245:
                     if pos[0] > 30 + x*70 and pos[0] < 90 + x*70:
                         self._setStone(x)
-        else:
-            if self.__restart.collidepoint(pos):
-                self.__init__()
+
+        if self.__restart.collidepoint(pos):
+            self.__init__()
+
+        if self.__undo.collidepoint(pos):
+            self._undo()
   
 
 
